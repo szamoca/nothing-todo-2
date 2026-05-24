d3bf2236f639bea07c2bb91b0df34bcc3171a9c8
szamøca <szamosidani@gmail.com>
2026-05-25 01:06:52 +0200

    feat: Define auth/login api endpoint

---
+++ server/api/auth/login.post.ts
@@ -0,0 +1,38 @@
+// import jwt from "jsonwebtoken";
+import { FetchError } from "ofetch";
+
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig();
+
+  const { username, password } = await readBody(event);
+
+  if (!(username && password)) {
+    throw createError({
+      statusCode: 400,
+      message: "Username and password are required",
+    });
+  }
+
+  try {
+    const response = await $fetch(`${config.public.apiBaseUrl}/auth/login`, {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+      expiresInMins: 3600,
+      credentials: "include",
+    });
+
+    return response;
+  } catch (error) {
+    if (error instanceof FetchError) {
+      throw createError({
+        statusCode: error.statusCode,
+        statusText: error.statusText,
+      });
+    }
+    throw error;
+  }
+});
